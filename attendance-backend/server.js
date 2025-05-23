45d2a65af9a364bdb80402f1180d5d6e5c744bc9
Joelwick <96626032+Joelwick@users.noreply.github.com>
2025-05-23 20:03:27 +0300

    omo

--- attendance-backend/server.js
+++ attendance-backend/server.js
@@ -33,4 +33,7 @@ app.use("/api/auth", authRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
