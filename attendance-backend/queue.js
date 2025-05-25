6f73ac739094ff40e67cbf9312e4733d461887ed
Joelwick <96626032+Joelwick@users.noreply.github.com>
2025-05-25 22:04:29 +0300

    lfg

--- attendance-backend/queue.js
+++ attendance-backend/queue.js
@@ -5,7 +5,8 @@ const Redis = require("ioredis");
 const redisConfig = {
   host: "redis-18022.c241.us-east-1-4.ec2.redns.redis-cloud.com",
   port: 18022, // replace with your Redis port
-  password: "izohOyAMd8zryJXAezC59lXoAFQ1PfPi", // if applicable
+  password: "izohOyAMd8zryJXAezC59lXoAFQ1PfPi",
+  tls: {} // if applicable
 };
 
 const redis = new Redis(redisConfig);
